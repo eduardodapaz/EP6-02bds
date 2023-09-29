5afccadfabcc3c30ff680cc88ec6938d95a13b5e
eduardodapaz <127500359+eduardodapaz@users.noreply.github.com>
2023-09-29 12:11:42 -0300

    Add files via upload

---
+++ relatividade/script.js
@@ -0,0 +1,11 @@
+function corrigir(){
+    var respostacerta = "correta";
+    var ressodtaUsuario = document.querySelector('input[name="resposta"] checked');
+
+if ( respostaUsuario){
+    if( respodtaUsuario.value === respostacerta){
+        document.getElementById
+    }
+}
+
+}
