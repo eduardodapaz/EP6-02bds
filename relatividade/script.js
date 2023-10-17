6792cccb43fab1ddd11a1fcefece791572734cb9
eduardodapaz <127500359+eduardodapaz@users.noreply.github.com>
2023-10-17 12:12:22 -0300

    Add files via upload

--- relatividade/script.js
+++ relatividade/script.js
@@ -3,4 +3,66 @@ function mudar(objeto, imagem) {
   }
 
 
-  
+  function inclui_nome(){
+      var nome_variavel = prompt("qual o seu nome");
+
+      if(nome_variavel){
+        document.getElementById("marcacao_nome").innerHTML = nome_variavel;
+      }
+      else{
+        alert("coloque o seu nome");
+        inclui_nome();
+      }
+
+  }
+
+  function funcao_resposta(){
+
+    var resp1 = document.querySelector('input[name="quest_1"]:checked');
+    var resp2 = document.querySelector('input[name="quest_2"]:checked');
+    var resp3 = document.querySelector('input[name="quest_3"]:checked');
+    var resp4 = document.querySelector('input[name="quest_4"]:checked');
+
+    var contador = 0;
+
+
+    if(resp1 && resp2 && resp3 && resp4){
+        if(resp1.value == "correta"){
+            document.getElementById("resp_correta_1").innerHTML = "Correta!"
+            contador = contador + 1;
+        }
+        else{
+            document.getElementById("resp_errada_1").innerHTML = "Errada!"
+
+        }
+        if(resp2.value == "correta"){
+            document.getElementById("resp_correta_2").innerHTML = "Correta!"
+            contador = contador + 1;
+        }
+        else{
+            document.getElementById("resp_errada_2").innerHTML = "Errada,!"
+
+        }
+        if(resp3.value == "correta"){
+            document.getElementById("resp_correta_3").innerHTML = "Correta!"
+            contador = contador + 1;
+        }
+        else{
+            document.getElementById("resp_errada_3").innerHTML = "Errada,!"
+
+        }
+        if(resp4.value == "correta"){
+            document.getElementById("resp_correta_4").innerHTML = "Correta!"
+            contador = contador + 1;
+        }
+        else{
+            document.getElementById("resp_errada_4").innerHTML = "Errada,!"
+
+        }
+        document.getElementById("quantos_acertos").innerHTML = "Você acertou " + contador + " /4 questões!";
+    }
+    else{
+        alert("não deixe questões em branco!")
+    }
+
+}
